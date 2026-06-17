084a582dd9e956abc2793c091faa8cca60d725f6
sameer-bagul <sameerbagul2004@gmail.com>
2026-06-17 11:57:46 +0530

    refactor: move Google Fonts import from layout head to globals CSS file

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,11 +29,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
       suppressHydrationWarning
     >
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Outfit:wght@400;500;600;700&display=swap" rel="stylesheet" />
-      </head>
+
       <body className="min-h-full flex flex-col" suppressHydrationWarning>
         <ClientProvider>
           {children}
